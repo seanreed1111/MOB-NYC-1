7117351bc02dd19b77202e8099a86998b70c907c
Sean Reed <seanreed1111@gmail.com>
2015-03-10 10:43:32 -0400

    adds mySwiftyJSON

---
+++ Lessons/Lesson16/MyPodSwiftyJson/Pods/Target Support Files/Pods-MyPodSwiftyJson/Pods-MyPodSwiftyJson-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyPodSwiftyJson : NSObject
+@end
+@implementation PodsDummy_Pods_MyPodSwiftyJson
+@end
---
+++ Lessons/Lesson16/MyPodSwiftyJson/Pods/Target Support Files/Pods-MyPodSwiftyJson/Pods-MyPodSwiftyJson-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_MyPodSwiftyJsonVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MyPodSwiftyJsonVersionString[];
+
