35e32b8a0f20f33b01122708fb50a9ec62a32d13
Sean Reed <seanreed1111@gmail.com>
2015-03-16 18:46:35 -0400

    adds parse starter project

---
+++ Lessons/Lesson19/LocationAndMapping/classMapProject/classMapProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  classMapProject
+//
+//  Created by Sean Reed on 3/11/15.
+//  Copyright (c) 2015 Sean Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
